bbfd530669b4e911bd90dd6749947cac2a3af760
JP-Pardinho <jppardinhor@gmail.com>
2024-07-11 20:26:43 -0300

    troca de so

---
+++ Aulas/aula001/ex001.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    int i = 0, num = [];
+    double = media;
+
+    for (i=0, i++, i <= 20) {
+        printf("Digite um numero: ");
+        scanf("%d", &num);
+        
+    }
+
+}
+return 0
